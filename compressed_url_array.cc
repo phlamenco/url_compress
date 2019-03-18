5eed5e0d954a9db2348ad0859bb8a4b04a65e81e
phlamenco <sweeliao@163.com>
2019-03-18 17:51:44 +0800

    add avl_tree

---
+++ compressed_url_array.cc
@@ -0,0 +1,4 @@
+// Author: sweeliao@163.com.
+// Date: 2019/03/18
+
+#include "compressed_url_array.h"
---
+++ utility.h
@@ -0,0 +1,16 @@
+// Author: sweeliao@163.com.
+// Date: 2019/03/18
+
+#ifndef URL_COMPRESS_UTILITY_H
+#define URL_COMPRESS_UTILITY_H
+
+namespace UrlCompression {
+
+// A utility function to get maximum of two integers
+inline int max(int a, int b) {
+
+}
+
+}
+
+#endif //URL_COMPRESS_UTILITY_H
